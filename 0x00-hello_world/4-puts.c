936ab87434c0bbfd6d82c375b8af1ab6812980d7
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-01 09:49:19 -0700

    puts f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main -> this is a function to print char
+ * return : success always 0
+ */
 int main(void)
 {
 char s[] = "\"Programming is like building a multilingual puzzle";
