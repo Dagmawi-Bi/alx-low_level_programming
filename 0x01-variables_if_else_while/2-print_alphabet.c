5a6828e0da88015ebe0bfe5d503f8769c9d85905
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-03 04:10:27 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: value always 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+putchar(10);
+return (0);
+}
