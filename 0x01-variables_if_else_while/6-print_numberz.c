58b84e882dc1f626b77716467f430086cef0eda6
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-03 05:07:57 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -prints all single digit numbers of base 10 starting from 0
+ * Return: value always 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar(10);
+return (0);
+}
