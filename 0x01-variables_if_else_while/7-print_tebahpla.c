49d6b26058fb63592366fcf1e72bc9de5818c89a
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-03 05:23:16 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * Return: value always 0
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+{
+putchar (i);
+}
+putchar (10);
+return (0);
+}
