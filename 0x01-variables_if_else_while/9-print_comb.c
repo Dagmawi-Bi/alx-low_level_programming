01d0dc88da82ae521eb0ac308a8035d68d4be375
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-03 05:50:05 -0700

    number with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: value always 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+if (i != 57)
+{
+putchar (44);
+putchar (32);
+}
+}
+putchar (10);
+return (0);
+}
