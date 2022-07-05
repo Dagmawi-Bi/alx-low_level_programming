34257c9ef85e983058d1f712c87f42d6f4b82e33
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-05 06:28:54 -0700

    deferenciation

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+ *  _islower -checks for lowercase character
+ *  @c: character argument
+ *  Return: value always 0 and 1 dependenig on condition
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
