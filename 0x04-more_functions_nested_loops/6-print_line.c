d38a28b54864a6cc0e873394c411e2dea7fedc9c
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-09 05:59:16 -0700

    for both

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+*print_line- draws a straight line in the terminal.
+*@n: integer parameter
+*/
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+_puchar ('\n');
+for (i >= 0; i <= n; i++)
+{
+_putchar (45);
+}
+_putchar ('\n');
+}
