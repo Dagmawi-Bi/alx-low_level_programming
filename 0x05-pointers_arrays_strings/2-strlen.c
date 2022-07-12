e738d35ac4c93fcfa71d555108e64f1a025e426d
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-12 04:26:02 -0700

    return the length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<string.h>
+/**
+ * _strlen- returns the length of a string
+ * @s:string pionter for the function
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
