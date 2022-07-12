133a0f422e3472edf34c065fde16a7f662872667
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-12 06:13:13 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<string.h>
+/**
+ * print_rev- prints a string, in reverse, followed by a new line.
+ * @s:strin to be printed
+ */
+void print_rev(char *s)
+{
+int i, j;
+j = 0;
+while (s[n] != '\0')
+n++;
+for (i = j - 1; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+_putchar ('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,8 @@
+#include"main.h"
+/**
+ * rev_string- reverses a string.
+ */
+void rev_string(char *s)
+{
+
+}
