b6c73e62b8e3883da915828dc9ec308f6ac189dd
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-12 07:36:18 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<string.h>
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: character parameter assigned
+ */
+void puts_half(char *str)
+{
+int i, j, k;
+i = strlen(str);
+if (i % 2 == 1)
+j = i / 2 + 1;
+else
+j = i / 2;
+for (k = j; k < i; k++)
+_putchar (str[k]);
+_putchar ('\n');
+}
