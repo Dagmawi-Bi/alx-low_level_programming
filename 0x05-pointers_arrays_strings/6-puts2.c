6d2525db90272dfa685d1d3863e7557b04d116f8
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-12 07:17:58 -0700

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include"main.h"
-#include<string.h>
+#include<string.ih>
 /**
  * puts2 - prints every other character of a string,
  * starting with the first character
@@ -10,6 +10,8 @@ void puts2(char *str)
 int i, j;
 i = strlen(str);
 for (j = 0; j < i; i += 2)
+{
 _putchar (str[j]);
+}
 _putchar ('\n');
 }
