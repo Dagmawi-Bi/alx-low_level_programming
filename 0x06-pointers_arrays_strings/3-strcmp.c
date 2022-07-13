a8e45b2e7f38b4aa4064abcbe2a4e67f576213c5
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-13 04:57:55 -0700

    string coparision

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: char parameter
+ * @s2: char parameter
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+int r;
+r = strcmp(s1, s2);
+return (r);
+}
