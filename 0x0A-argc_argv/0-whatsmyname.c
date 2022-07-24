722f437674469b64ff4da6a585ffcbc4c3a15a25
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-24 05:40:13 -0700

    print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prints its name, followed by a new line
+ * @argc: integer parameter
+ * @argv: charater parameter
+ * Return: valu 0
+ */
+int main (int argc, char *argv[])
+{
+printf ("%s\n", *argv);
+return (0);
+}
