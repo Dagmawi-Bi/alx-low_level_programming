71e38c55b60d4405e4a9b4a4af1fd0e8873f95f2
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-24 08:04:25 -0700

    printing index

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: integer parameter
+ * @argv: an arrey of a command listed
+ * Return: value 0
+ */
+int main(int argc, char argv[])
+{
+for (int i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
