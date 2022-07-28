a8f8a9e84542fa15a95d990f061ff0b46a4a1e24
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-07-28 04:37:28 -0700

    fix for concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,10 +24,10 @@ return (NULL);
 len = 0;
 for (index = 0; s1[index]; index++)
 concat[len++] = s1[index];
-for (index = 0; s2[index]; &&index < n; index++)
+for (index = 0; s2[index] && index < n; index++)
 {
 concat[len++] = s2[index];
 }
-concat[len] = '\0'
+concat[len] = '\0';
 return (concat);
 }
