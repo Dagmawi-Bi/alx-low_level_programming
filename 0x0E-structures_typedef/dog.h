b9776b4057cad98521de7228a67084971212a9a1
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-08-02 05:30:06 -0700

    struct fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
-#ifndef DOG_H
-#define DOG_H
-#define struct dog
+#ifndef STRTYP
+#define STRTYP
+/**
+ * struct dog - structure of a dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ * @name: name of the dog
+ */
+struct dog
 {
 char *name;
 char *owner;
 float age;
 };
+typedef struct dog dog_t;
+void int_dog(struct dog *d, char *name, char *owner, float age);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, char *owner, float age);
+void free_dog(dog_t *d);
 #endif
