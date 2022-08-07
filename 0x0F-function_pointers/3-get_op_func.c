e93a279139031a50827d0f3d25087c7d5fdae1a0
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-08-07 05:58:33 -0700

    fix up

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,7 +4,7 @@
  * @s: oprator used
  * Return: NULL or the function based on function
  */
-int (*get_op_func(char *s))(int int)
+int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
 {"+", op_add},
@@ -12,16 +12,17 @@ op_t ops[] = {
 {"*", op_mul},
 {"/", op_div},
 {"%", op_mod},
-{NULL, NULL},
+{NULL, NULL}
 };
-int i = 0;
+int i;
+i = 0;
 while (i < 5)
 {
 if (s[0] == ops[i].op[0])
 {
 return (ops[i].f);
 }
-i++
+i++;
 }
 return (NULL);
 }
