cd13be7aa5c3e19f2208b7c2e008b4806a15e14c
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-08-07 07:47:33 -0700

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include"variadic_functions.h"
+
 #include<stdarg.h>
 /**
  * sum_them_all - returns the sum of all its parameters
@@ -7,13 +7,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+unsigned int sum = 0, i;
+va_list arglist;
 if (n == 0)
 return (0);
-va_list ap;
-va_start(ap, n);
-int i = 0, sum = 0;
+va_start(arglist, n);
 for (i = 0; i < n; i++)
-sum += va_arg(ap, const unsigned int);
-va_end(ap);
+{
+sum += va_arg(arglist, int);
+}
+va_end(arglist);
 return (sum);
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,12 @@
+#include"variadic_functions.h"
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: a pointer
+ * @n: unsigned integer parameter
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
