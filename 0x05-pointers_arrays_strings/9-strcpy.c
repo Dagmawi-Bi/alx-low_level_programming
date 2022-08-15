1ae28f9f1bc9063951b0a4331f4d6fc61a6f320c
Dagmawi-Bi <dagmawibirhanukassaye@gmail.com>
2022-08-15 03:50:49 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,21 @@
-#include"main.h"
+#include "main.h"
+
 /**
- * *_strcpy- copies the string pointed to by src
+ * _strcpy - Copiar el contenido de uno a otra variable
+ * @dest: This is destiny
+ * @src: This is the copia
+ * Return: This return copy
  */
+
 char *_strcpy(char *dest, char *src)
 {
-
+char *start = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
 }
